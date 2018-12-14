8bbc5223c62d8889e3bfa7efc7106045cc81bdb3
Samuel <samueleguino97@gmail.com>
2018-12-14 17:27:44 -0400

    se quitó la propiedad de check box, implementacion lectura (firebase)

--- src/App.js
+++ src/App.js
@@ -21,48 +21,51 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: {
-        curr: '',
-        isChecked:false
-      },
+      term: '',
       items: []
     };
     this.onClick=this.onClick.bind(this);
+    
   }
+  componentDidMount(){
+    let data=[];
+    const tasksRef = database.ref('/tasks');
+    tasksRef.on('value',function(snapshot){
+      snapshot.forEach(function(chSnapshot){
+        data.push(chSnapshot.val());
+      });
+    });
+    this.setState({
+      items: data
+    });
+    
+  }
+  
 
   onChange = (event) => {
-    this.setState({ term: {
-      curr: event.target.value,
-      isChecked:false,
-    } });
+    this.setState({ term: event.target.value
+    } );
   }
 
-  onClick = (event)=>{
+ 
+  onClick(e){
+    const nI=this.state.items;
+    nI.splice(nI.indexOf(e.target.value),1);
+    setTimeout(()=>{
     
-    
-    this.setState({term:{
-      curr: this.state.term.curr,
-      isChecked: !this.state.term.isChecked
-    }})
-
-    
-  }
-  componentDidUpdate(){
-    const taskRef = database.ref('/tasks');
-    taskRef.update(this.state.term);
+    this.setState({
+      items: nI,
+    });},1000);
   }
 
   onSubmit = (event) => {
     event.preventDefault();
-    if(this.state.term.curr){
+    if(this.state.term){
     const itemsRef=database.ref('/tasks');
     itemsRef.push(this.state.term);
 
     this.setState({
-      term: {
-        curr: '',
-        isChecked:false
-      },
+      term: '',
       items: [...this.state.items, this.state.term]
     });
   }
@@ -74,7 +77,7 @@ export default class App extends Component {
       <div className="wrapper">
       <h1 style={{color: 'lightblue'}}>To-Do List</h1>
         <form className="App" onSubmit={this.onSubmit}>
-          <input value={this.state.term.curr} onChange={this.onChange} />
+          <input value={this.state.term} onChange={this.onChange} />
           <button>Add Task</button>
         </form>
         <List onClick={this.onClick} items={this.state.items} />
--- src/List.js
+++ src/List.js
@@ -1,19 +1,13 @@
 import React from 'react';
 
 export class List extends React.Component{
-    constructor(props){
-        super(props);
-        
-        this.handleClicK=this.handleClicK.bind(this);
-    }
-    handleClicK(event){
-        this.props.onClick();
-    }
+    
+    
     render(){
         return (<ul>
             {
-              this.props.items.map((item, index) => <form key={index}><label for={item.curr}>{item.curr}</label>
-              <input id={item.curr} onClick={this.handleClicK} type="checkbox"></input></form>)
+              this.props.items.map((item, index) => <form key={index}><label htmlFor={item}>{item}</label>
+              <input onClick={this.props.onClick}value={item} id={item} type="checkbox"></input></form>)
             }
           </ul>);
     }
