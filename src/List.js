aaf41275040d95f6553d0e707353a1cdd176253b
Samuel <samueleguino97@gmail.com>
2018-12-17 11:22:17 -0400

    fixed checkbox bug

--- src/List.js
+++ src/List.js
@@ -6,8 +6,8 @@ export class List extends React.Component{
     render(){
         return (<ul>
             {
-              this.props.items.map((item, index) => <form key={index}><label htmlFor={item}>{item}</label>
-              <input unchecked onClick={this.props.onClick}value={item} id={item} type="checkbox"></input></form>)
+              this.props.items.map((item, index) => <form key={item+'_'+index}><label htmlFor={item}>{item}</label>
+              <input onClick={this.props.onClick}value={item} id={item} type="checkbox"></input></form>)
             }
           </ul>);
     }
